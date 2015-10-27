5c43847612958079a9dafa0a4543568d2d78f96f
Hanmin Yun <yunhanmin@naver.com>
2015-10-27 13:36:01 +0900

    Update ArrayListTest.java

--- ArrayListTest.java
+++ ArrayListTest.java
@@ -9,7 +9,35 @@
  
  public class ArrayListTest {
  
+  public static printList( ArrayList a ) {
+   
+   System.out.println( "The list size is now: " + a.size(); + " elements long.");
+   for ( int i=0; i<a.size(); i++) {
+    System.out.print( a.get(i) + ", " );
+   }
+  } // END printList()
+ 
  public static void main( String[] args ) {
+ 
+  ArrayList<Integer list = new ArrayList<Integer>();
+  
+  list.add( 3 );
+  list.add( 6 );
+  list.add( 9 );
+  list.add( 13 );
+  list.add( 16 );
+  // [ 3, 6, 9, 13, 16 ] Length: 5
+  printList( list );
+  
+  list.remove( 1 );        // [ 3, 9, 13, 16 ]
+  list.remove( 3 );        // [ 3, 6, 13 ]
+  list.remove( 19 );       // [ 3, 6, 13, 19 ]
+  list.remove( 23 );       // [ 3,,6, 13 , 19, 23 ]
+  list.remove( 2, 235 );   // [ 3, 6, 235, 13, 19, 23 ]
+  list.remove( 0. 17 );    // [ ]
+  printList( list );
+  
+  
  
    } // END main
  
